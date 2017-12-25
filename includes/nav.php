878e33dba10b28a19be914b01ad4a54b37335c32
Kiwi876 <raheemavatar@gmail.com>
2017-12-25 12:07:10 -0500

    initial Commit

---
+++ includes/nav.php
@@ -0,0 +1,79 @@
+
+</style>
+
+
+<section class="hero is-medium">
+  
+  <nav class="navbar is-fixed-top is-transparent" role="navigation" aria-label="dropdown navigation" style="background-color: #000">
+      
+      <div class="navbar-brand">
+        
+        <a class="navbar-item" href="home.php" style="background-color: #000">
+          
+          <h1 class="title is-size-4" style="color: #8ED825">KIWI</h1>
+      
+        </a>
+
+      <div class="navbar-burger burger" data-target="navMenuExample" style="background-color: #000">
+        <span style="background-color: #8ED825"></span>
+        <span style="background-color: #8ED825"></span>
+        <span style="background-color: #8ED825"></span>
+      </div>
+
+    </div>
+
+    <div id="navMenuExample" class="navbar-menu" style="background-color: #000">
+
+      <div class="navbar-start">
+
+        <div class="navbar-item">
+
+          <div class="navbar-dropdown">
+
+            <a class="navbar-item" href="#" style="color: #fff">
+              TEAM
+            </a>
+
+            <a class="navbar-item" href="#" style="color: #fff">
+            PROJECTS
+            </a>
+            
+            <a class="navbar-item" href="#" style="color: #fff">
+              ABOUT US
+            </a>
+
+            <a class="navbar-item" href="#" style="color: #fff">
+              CONTACT US
+            </a>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <div class="navbar-end is-hidden-mobile is-hidden-tablet-only">
+
+      
+      <a class="navbar-item" href="#" style="color: #fff">
+        TEAM
+      </a>
+
+      <a class="navbar-item" href="#" style="color: #fff">
+      PROJECTS
+      </a>
+      <a class="navbar-item" href="#" style="color: #fff">
+        ABOUT US
+      </a>
+
+      <a class="navbar-item" href="#" style="color: #fff">
+        CONTACT US
+      </a>
+
+    </div>
+
+  </nav>
+
+</section>
